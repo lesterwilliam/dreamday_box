3c92357a455b7ddbef88e8529dc93bf78b830eb6
Adrian Schwizgebel <adrian.schwizgebel@gmail.com>
2020-02-21 15:58:46 +0100

    Update test_clock_seg_new_rtc_lib.ino

--- test_clock_seg_new_rtc_lib/test_clock_seg_new_rtc_lib.ino
+++ test_clock_seg_new_rtc_lib/test_clock_seg_new_rtc_lib.ino
@@ -10,26 +10,28 @@
 #define CS_DOT A2
 
 // MAX7219 SPI LED Driver
-#define MAX7219_TEST 0x0f // in real code put into a .h file
-#define MAX7219_BRIGHTNESS 0x0a // in real code put into a .h file
-#define MAX7219_SCAN_LIMIT 0x0b // in real code put into a .h file
-#define MAX7219_DECODE_MODE 0x09 // in real code put into a .h file
-#define MAX7219_SHUTDOWN 0x0C // in real code put into a .h file
+#define MAX7219_TEST        0x0f
+#define MAX7219_BRIGHTNESS  0x0a
+#define MAX7219_SCAN_LIMIT  0x0b
+#define MAX7219_DECODE_MODE 0x09
+#define MAX7219_SHUTDOWN    0x0C
 
 RTClib RTC;
 
 const long hammock_unixtime = 1403913000;
 const long correction_unixtime = 1638043;
 
-void maxTransferCMD(uint8_t address, uint8_t value) {  
+const int test_matrix[8] = {0x80, 0x40, 0x20, 0x10, 0x08, 0x04, 0x02, 0x01};
+
+void maxTransferCMD(uint8_t chipSel, uint8_t address, uint8_t value) {  
 uint8_t i;
 
-  digitalWrite(CS_SEG, LOW);   
+  digitalWrite(chipSel, LOW);   
   SPI.transfer(address);      // Send address.
   SPI.transfer(value);        // Send the value.
   SPI.transfer(address);      // Send address. 
   SPI.transfer(value);        // Send the value.
-  digitalWrite(CS_SEG, HIGH); // Finish transfer.
+  digitalWrite(chipSel, HIGH); // Finish transfer.
 }
 
 void writeDigits(int value){
@@ -38,9 +40,19 @@ void writeDigits(int value){
     SPI.transfer(i);
     SPI.transfer(value % 10);
     digitalWrite(CS_SEG, HIGH);
-  value = value / 10;
+    value = value / 10;
   }
 }
+
+void writeMatrix(int value[]){
+  for(int i = 0; i < 8; i++){
+    digitalWrite(CS_DOT, LOW);
+    SPI.transfer(i + 1);
+    SPI.transfer(value[i]);
+    digitalWrite(CS_DOT, HIGH);
+  }
+}
+
 /*
 void setRTC(){
     RTC.setYear(20);
@@ -73,22 +85,31 @@ void setup() {
   Wire.begin();
   //setRTC();
   
-  // Run test - All LED segments lit. 
-  //maxTransferCMD(MAX7219_TEST, 0x01);  delay(1000);  
-  maxTransferCMD(MAX7219_TEST, 0x00);        // Finish test mode.
-  maxTransferCMD(MAX7219_DECODE_MODE, 0x1F); // Enable BCD mode. 
-  maxTransferCMD(MAX7219_BRIGHTNESS, 0x00);  // Use lowest intensity. 
-  maxTransferCMD(MAX7219_SCAN_LIMIT, 0x1F);  // Scan all digits.
-  maxTransferCMD(MAX7219_SHUTDOWN, 0x01);    // Turn on chip.
+  // Init 7-seg driver
+  //maxTransferCMD(CS_DOT, MAX7219_TEST, 0x01);  delay(1000);  
+  maxTransferCMD(CS_SEG, MAX7219_TEST, 0x00);        // Finish test mode.
+  maxTransferCMD(CS_SEG, MAX7219_DECODE_MODE, 0x1F); // Enable BCD mode. 
+  maxTransferCMD(CS_SEG, MAX7219_BRIGHTNESS, 0x00);  // Use lowest intensity. 
+  maxTransferCMD(CS_SEG, MAX7219_SCAN_LIMIT, 0xFF);  // Scan all digits.0x1F
+  maxTransferCMD(CS_SEG, MAX7219_SHUTDOWN, 0x01);    // Turn on chip.
 
+  // Init dot-matrix driver
+  //maxTransferCMD(CS_DOT, MAX7219_TEST, 0x01);  delay(1000);  
+  //maxTransferCMD(CS_DOT, MAX7219_TEST, 0x00);        // Finish test mode.
+  maxTransferCMD(CS_DOT, MAX7219_DECODE_MODE, 0x00); // Enable BCD mode. 
+  maxTransferCMD(CS_DOT, MAX7219_BRIGHTNESS, 0x00);  // Use lowest intensity. 
+  maxTransferCMD(CS_DOT, MAX7219_SCAN_LIMIT, 0xFF);  // Scan all digits.
+  maxTransferCMD(CS_DOT, MAX7219_SHUTDOWN, 0x01);    // Turn on chip.
 }
 
 void loop() {
   DateTime now = RTC.now();
   
   writeDigits(getYearSince());
+  delay(2000);
   writeDigits(now.minute());
-  delay(1000);
+  delay(2000);
+  writeMatrix(test_matrix[8]);
 }
 
 
